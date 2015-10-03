d21fd910ae38519f5b4cfee7ddb012bbd8ff3aa9
Martín <mpugatelecouvigo@gmail.com>
2015-10-03 11:46:03 +0200

    Commit de prueba

--- src/hola_pablo/Hola_Pablo.java
+++ src/hola_pablo/Hola_Pablo.java
@@ -5,6 +5,6 @@ public class Hola_Pablo {
 	public static void main(String[] args) {
 		System.out.println("Hola Puga");
 
-		System.out.println("Funciona");
+		System.out.println("Funciona?");
 	}
 }
